7b49aa709ac67073f66e399c22c335103150e8fc
David Sundelius <david.sundelius@proagile.se>
2022-03-11 02:28:30 +0100

    Change url again to match new ghpages settings

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === "production" ? "/e2etestworkshop/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/e2etestworkshop/dist/" : "/",
   transpileDependencies: true
 })
